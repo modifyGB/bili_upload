e3835007dc18fc3308d898dc487cd9d76f9d64ad
RHT <2782299413@qq.com>
2021-02-10 22:35:07 +0800

    first

--- main2.py
+++ main2.py
@@ -4,7 +4,8 @@ import json
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        ul = upload(json.loads(sys.argv[1]))
-        ul.upload()
+        with open(sys.argv[1],'r+',encoding='utf-8') as file:
+            ul = upload(json.loads(file.read()))
+            ul.upload()
     else:
         print('你没有传配置文件')
